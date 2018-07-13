b4445730c9007e5a29bffb09d98e7d4dcf74a5f1
Jon Wood <jon@blankpad.net>
2018-07-13 17:23:34 +0100

    Move assets around

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,4 +1,7 @@
 const { environment } = require('@rails/webpacker')
+const sass = require('./loaders/sass')
+
+environment.loaders.append('sass', sass)
 
 // resolve-url-loader must be used before sass-loader
 environment.loaders.get('sass').use.splice(-1, 0, {
