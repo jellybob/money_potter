11689c8c7a89a5a9efc6025bb41a6fdc751aa10f
Jon Wood <jon@blankpad.net>
2018-07-15 13:49:59 +0100

    Add search for pots to the overview

---
+++ app/javascript/controllers/pot_list_controller.js
@@ -0,0 +1,43 @@
+// Visit The Stimulus Handbook for more details
+// https://stimulusjs.org/handbook/introduction
+//
+// This example controller works with specially annotated HTML like:
+//
+// <div data-controller="hello">
+//   <h1 data-target="hello.output"></h1>
+// </div>
+
+import { Controller } from "stimulus"
+
+export default class extends Controller {
+  static targets = [ "pot", "form", "query" ]
+
+  initialize() {
+    this.formTarget.classList.remove("d-none");
+    this.resetFilter();
+  }
+
+  resetFilter() {
+   for (let pot of this.potTargets) { pot.classList.remove("d-none"); }
+  }
+
+  applyFilter(query) {
+    for (let pot of this.potTargets) {
+      if (pot.getAttribute("data-name").includes(query.toLowerCase())) {
+        pot.classList.remove("d-none");
+      } else {
+        pot.classList.add("d-none");
+      }
+    }
+  }
+
+  search() {
+    let query = this.queryTarget.value;
+    if (query === '') {
+      this.resetFilter();
+    } else {
+      this.applyFilter(query);
+    }
+  }
+}
+
