2145a6dc55f91d725962c52853e9f967275b3a35
Jon Wood <jon@blankpad.net>
2018-07-26 07:19:39 +0100

    Resolve accessibility and speed issues

--- app/javascript/controllers/payment_form_controller.js
+++ app/javascript/controllers/payment_form_controller.js
@@ -5,7 +5,7 @@ export default class extends Controller {
 
   connect() {
     this.element.classList.remove("was-validated");
-    this.element.querySelector("#pot_id").focus();
+    this.element.querySelector("#payment_pot_id").focus();
   }
 
   submit(event) {
